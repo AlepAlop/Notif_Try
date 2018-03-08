b643044fbaff9c29aac192de53d43086cd3c5cfd
AlepAlop <sains2468@yahoo.com>
2018-03-08 17:30:44 +0800

    #1.4.1 Wrong credential

--- conn.php
+++ conn.php
@@ -3,7 +3,7 @@
 //MySQLi Procedural
 $conn = mysqli_connect("localhost","root","","notif");
 if (!$conn) {
-	$conn = mysqli_connect("host=eu-cdbr-west-02.cleardb.net dbname=heroku_ccbf80b0e334f69 user=bd0253e53a1f77 password=3012148e");
+	$conn = mysqli_connect("eu-cdbr-west-02.cleardb.net", "bd0253e53a1f77", "3012148e", "heroku_ccbf80b0e334f69");
 	if(!$conn){
 		die("Connection failed: " . mysqli_connect_error());
 	}
