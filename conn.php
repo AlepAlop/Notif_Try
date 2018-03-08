51e1434308a0434f22c14b37eafa6614e0154d0a
AlepAlop <sains2468@yahoo.com>
2018-03-08 14:33:04 +0800

    #1.3 Try again

--- conn.php
+++ conn.php
@@ -3,9 +3,9 @@
 //MySQLi Procedural
 $conn = mysqli_connect("localhost","root","","notif");
 if (!$conn) {
-	$conn = mysqli_connect("ec2-54-247-95-125.eu-west-1.compute.amazonaws.com","bdtcmrtidvrxym","2a668b0109111153548e54319e9ad1b185a1ca66e12df6438de4ac5731615bf7","d14pphlglvhq83");
+	$conn = pg_connect("host=ec2-54-247-95-125.eu-west-1.compute.amazonaws.com dbname=d14pphlglvhq83 user=bdtcmrtidvrxym password=2a668b0109111153548e54319e9ad1b185a1ca66e12df6438de4ac5731615bf7");
 	if(!$conn){
-		die("Connection failed: " . mysqli_connect_error());
+		die("Connection failed: " . pg_connect_error());
 	}
 }
  
